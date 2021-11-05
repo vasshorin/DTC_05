7d38b2d3cbecfffd32f00f8c87fb85e6a677da61
Ryo <66569784+UnD-Ryo@users.noreply.github.com>
2021-11-05 00:26:32 -0700

    Added login page

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDFV2ZrtMAs2xj61w8zcCx6hmsoMB6qYYw",
+  authDomain: "dtc-5-project.firebaseapp.com",
+  projectId: "dtc-5-project",
+  storageBucket: "dtc-5-project.appspot.com",
+  messagingSenderId: "33147194302",
+  appId: "1:33147194302:web:83b4740113e2f963b387ef",
+  measurementId: "G-7SCKFHGKEF"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+  
