3d740d2490f42e4ae46f4c6a53eec7a7718b0f2e
Vasily <vshorin@my.bcit.ca>
2021-11-19 01:49:10 -0800

    refactor custom search, add data to pages

--- scripts/searchCountry.js
+++ scripts/searchCountry.js
@@ -1,24 +1,29 @@
+const collectionRef = db.collection('Country')
+   
 let submitButton = document.getElementById("submitBtn")
 let Country = document.getElementById("country")
 let Nationality = document.getElementById("nationality")
+let div1 = document.getElementById('test')
 
 function searchCountry(){
-	console.log(Country.value)
-    console.log(Nationality.value)
-	if(Country.value == 'Australia' && Nationality.value == 'Canada'){
-		console.log("asdsa")
-		submitButton.setAttribute("href",'./australia-visa.html')
+  console.log(Country.value)
+  console.log(Nationality.value)
+  console.log(collectionRef)
+  console.log(collectionRef.doc(Country.value).get().then((snapshot) => {
+    console.log(snapshot.data())
+	const allowed = snapshot.data()['Allowed']
+	const notAllowed = snapshot.data()['Not_allowed']
+	const link = snapshot.data()['Link']
+	const link_no = snapshot.data()['Link_no']
+	const exists = allowed.indexOf(Nationality.value)
+	if (exists == -1){
+		submitButton.setAttribute("href",link)
+		// div1.textContent = "ASDJNAFJIASDBFISDBFIASDBFABSD"
+	} else {
+		submitButton.setAttribute("href",link_no)
 	}
-	if(Country.value == 'Canada'){
-		submitButton.setAttribute("href",'./canada.html')
-	}
-	if(Country.value == 'New Zealand'){
-		submitButton.setAttribute("href",'./newZealand.html')
-	}
-	if(Country.value == 'United Kingdom'){
-		submitButton.setAttribute("href",'./uk.html')
-	}
-	if(Country.value == 'United States'){
-		submitButton.setAttribute("href",'./usa.html')
-	}
+	console.log(exists)
+}).catch((value) => {
+    console.log(value)
+  }))
 }
