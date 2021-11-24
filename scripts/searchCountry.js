ac822528076d52f7fa9bb929b84f5dc055f68a34
vasshorin <vshorin@my.bcit.ca>
2021-11-23 22:12:41 -0800

    Modify searching mechanism

--- scripts/searchCountry.js
+++ scripts/searchCountry.js
@@ -11,17 +11,20 @@ function searchCountry(){
 //   console.log(collectionRef)
   console.log(collectionRef.doc(Country.value).get().then((snapshot) => {
     // console.log(snapshot.data())
-	const visa = snapshot.data()['Need']
+	// const visa = snapshot.data()['Need']
+	const visa = "require1 visa"
+	const noVisa = snapshot.data()['No_need']
 	const allowed = snapshot.data()['Allowed']
 	const notAllowed = snapshot.data()['Not_allowed']
 	const link = snapshot.data()['Link']
 	const link_no = snapshot.data()['Link_no']
 	const exists = allowed.indexOf(Nationality.value)
 	if (exists == -1){
-		submitButton.setAttribute("href",link)
+		submitButton.setAttribute("href",link_no)
 		localStorage.setItem('div1', visa)
 	} else {
 		submitButton.setAttribute("href",link_no)
+		localStorage.setItem('div1', noVisa)
 	}
 	console.log(exists)
 }).catch((value) => {
