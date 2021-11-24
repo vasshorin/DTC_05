a6e4741855a963e1e895bfb998086bdc8a919134
vasshorin <vshorin@my.bcit.ca>
2021-11-23 22:42:32 -0800

    refactor

--- scripts/searchCountry.js
+++ scripts/searchCountry.js
@@ -11,8 +11,8 @@ function searchCountry(){
 //   console.log(collectionRef)
   console.log(collectionRef.doc(Country.value).get().then((snapshot) => {
     // console.log(snapshot.data())
-	// const visa = snapshot.data()['Need']
-	const visa = "require1 visa"
+	const visa = snapshot.data()['Need']
+	// const visa = "require visa"
 	const noVisa = snapshot.data()['No_need']
 	const allowed = snapshot.data()['Allowed']
 	const notAllowed = snapshot.data()['Not_allowed']
@@ -20,7 +20,7 @@ function searchCountry(){
 	const link_no = snapshot.data()['Link_no']
 	const exists = allowed.indexOf(Nationality.value)
 	if (exists == -1){
-		submitButton.setAttribute("href",link_no)
+		submitButton.setAttribute("href",link)
 		localStorage.setItem('div1', visa)
 	} else {
 		submitButton.setAttribute("href",link_no)
